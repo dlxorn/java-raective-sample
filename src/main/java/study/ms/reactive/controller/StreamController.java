b6251f2ec9ede47d9543458a817c890410a41043
Tae-Gu Lee <tglee@fin2b.com>
2021-05-30 13:54:54 +0900

    replay-sink와 backpressure-sink의 차이점

--- src/main/java/study/ms/reactive/controller/StreamController.java
+++ src/main/java/study/ms/reactive/controller/StreamController.java
@@ -14,22 +14,25 @@ import study.ms.reactive.dto.MessageDTO;
 @RequestMapping("/stream")
 public class StreamController {
 
-  //many는 flux로 값을 전달한다는 듯?
-  final Sinks.Many sink;
+  final Sinks.Many<MessageDTO> backpressureBufferSink ;
+  final Sinks.Many<MessageDTO> replaySink ;
+  //차이점
+  //backpressureBufferSink -> 첫번째 구독자를 제외하면 그 뒤 구독자들은 자신이 구독 시점의 이후 정보만 받아볼 수 있다.
+  //replaySink -> 설정한 리미트 갯수대로 구독 시점의 정보도 받아볼 수 있다.
+
+
 
   public StreamController() {
-                       //TODO 메서드의 의미가 무엇인지 확인할 것
-    this.sink =  Sinks.many().multicast().onBackpressureBuffer();
-
+    this.backpressureBufferSink =  Sinks.many().multicast().onBackpressureBuffer();
+    this.replaySink =  Sinks.many().replay().limit(5);
   }
-
-
 
   @GetMapping("/send")
   public boolean addValue(){
     //특이한 것은 제네릭을 쓰지 않아도, 아래 객체가 json 형태로 파싱되어 전달된다.
     //안에서 json으로 변경해주는 로직이 있을 듯
-    sink.tryEmitNext(new MessageDTO());
+    backpressureBufferSink.tryEmitNext(new MessageDTO());
+    replaySink.tryEmitNext(new MessageDTO());
 
     return true;
   }
@@ -42,11 +45,16 @@ public class StreamController {
   //가능하게 된다.
   //참고)https://stackoverflow.com/questions/51370463/spring-webflux-flux-how-to-publish-dynamically
   //책 참고 292쪽(책에서는 ReplayProcessor가 존재하나 depreacted 되었음)
-  @GetMapping(value="/receive" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<String> stream(){
-                                  //TODO ServerSentEvent event도 뭔지 확인 필요. 안써도 객체 결과는 나오나..확인이 필요하다
-    return sink.asFlux().map(e -> ServerSentEvent.builder(e).build());
+  @GetMapping(value="/receive1" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<Object> streamBackPressure(){
+   //TODO ServerSentEvent event도 뭔지 확인 필요. 안써도 객체 결과는 나오나..왜 쓰는지 확인 필요.
+    return backpressureBufferSink.asFlux().map(e -> ServerSentEvent.builder(e).build());
   }
 
+  @GetMapping(value="/receive2" ,produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<Object> streamReplay(){
+    //TODO ServerSentEvent event도 뭔지 확인 필요. 안써도 객체 결과는 나오나..왜 쓰는지 확인 필요.
+    return replaySink.asFlux().map(e -> ServerSentEvent.builder(e).build());
+  }
 
 }
